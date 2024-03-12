c91b8b340e6221caad55a192cdb23e3ab0627e47
al-xndram <amaftei@ocadu.ca>
2024-03-12 12:51:38 -0400

    first class apple

--- JS-LESSONS/week-01/js/script.js
+++ JS-LESSONS/week-01/js/script.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 
 let message = 'hello world';
@@ -23,6 +24,30 @@ $(".student").hover(function(){
     }
     
 
+=======
+let message = "hello world";
+// to get to the console, type in CTRL+SHIFT+J
+console.log(message)
+
+//everything is .something()
+// () in function(){} is for passing variables through. if theres nothing its an anonymous function
+// $(".student").click(function(){
+//     $("body").addClass("blueBG") // .css(key (thing that changes), what to change)
+// })
+
+
+$(".student").hover(function(){
+    if ( ! $(this).hasClass("hovered")) { // ! is the opposite 
+        let name = $(this).text()
+    let interaction = $(this).attr("data-inter")
+    let newName = name +" - "+interaction;
+    $(this)
+    .text(newName)
+    .addClass("hovered")
+    }
+
+    
+>>>>>>> Stashed changes
 })
 
 
@@ -30,6 +55,7 @@ $(".student").hover(function(){
 
 
 
+<<<<<<< Updated upstream
 
 
 
@@ -46,8 +72,31 @@ $(".student").click(function(){
         // alert("wait, you're not River. You're "+name)
     }
     
+=======
+$(".student").click(function(){
+    //($("body").toggleClass("blueBG")) 
+
+    let name = $(this).text() // THIS = the original thing it looks for = ".student"
+    // alert(name)
+
+    if (name == "River Carley") {
+        $("body").toggleClass("blueBG")
+        $(".apple").css("display","block")
+            } else {
+        alert("wait you're not River, you're " + name)
+    }
+>>>>>>> Stashed changes
 })
 
 $(".circle").click(function(){
     $(this).toggleClass('active')
+<<<<<<< Updated upstream
+=======
+})
+
+$(document).scroll(function(){
+    let fromTop = $(document).scrollTop()/10
+    console.log(fromTop);
+    $(".second-hand").css("transform", "rotate("+fromTop+"deg)")
+>>>>>>> Stashed changes
 })
